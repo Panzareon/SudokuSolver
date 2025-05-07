25b0cefb332deb00d0a19558c179600e65dadc80
Panzareon <fogtociag@web.de>
2025-05-07 21:05:10 +0200

    Use separate control to display tiles

---
+++ SudokuSolver.UI/ViewModels/DesignTimeTileViewModel.cs
@@ -0,0 +1,16 @@
+﻿using SudokuSolver.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver.UI.ViewModels
+{
+	internal class DesignTimeTileViewModel : TileViewModel
+	{
+		public DesignTimeTileViewModel() : base(Board.Parse("1"), 0, 0)
+		{
+		}
+	}
+}
---
+++ SudokuSolver.UI/Views/Tile.axaml.cs
@@ -0,0 +1,14 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SudokuSolver.UI.Views
+{
+	public partial class Tile : UserControl
+	{
+		public Tile()
+		{
+			InitializeComponent();
+		}
+	}
+}
